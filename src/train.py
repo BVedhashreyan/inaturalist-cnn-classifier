dc74bc3333a5b403bb7205629e1f15af3ab7ab02
BVedhashreyan <vedhashreyan85@gmail.com>
2026-06-06 12:19:01 +0530

    train.py

--- src/train.py
+++ src/train.py
@@ -65,13 +65,32 @@ def validate(model, val_loader, criterion, device):
 
 def main():
     wandb.init(
-        project="inaturalist-cnn-classifier"
+        project="inaturalist-cnn-classifier",
+        config={
+            "filters": [32,64,128,64,32],
+            "batch_size": 32,
+            "learning_rate": 0.001,
+            "epochs": 3,
+            "dropout": 0.0,
+            "batch_norm": False,
+            "activation": "relu"
+        }
     )
-    model = CNN(filters=[32,64,128,64,32])
+
+    config = wandb.config
+
+    model = CNN(
+        filters=config.filters,
+        dropout=config.dropout,
+        batch_norm=config.batch_norm,
+        activation=config.activation
+    )
 
     total_params = sum(p.numel() for p in model.parameters())
     print(total_params)
-
+    wandb.log({
+        "total_parameters": total_params
+    })
     
     device = torch.device(
         "cuda" if torch.cuda.is_available() else "cpu"
@@ -80,7 +99,7 @@ def main():
 
     model = model.to(device)
 
-    learning_rate = 0.001
+    learning_rate = config.learning_rate
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate, weight_decay=1e-4)
     
@@ -91,16 +110,28 @@ def main():
         train_path = "data/inaturalist_12K/train"
         val_path = "data/inaturalist_12K/val"
 
-    train_loader, val_loader, test_loader = get_loaders(train_path, val_path, batch_size=32)
+    train_loader, val_loader, test_loader = get_loaders(train_path, val_path, batch_size=config.batch_size)
 
-    num_epochs = 3
+    num_epochs = config.epochs
     best_val_acc = 0
     os.makedirs("outputs", exist_ok=True)
+
+    if os.path.exists("/content"):
+        os.makedirs(
+            "/content/drive/MyDrive/inaturalist-cnn-classifier",
+            exist_ok=True
+        )
+    
+    save_path = "outputs/best_model.pth"
+    if os.path.exists("/content"):
+        save_path = "/content/drive/MyDrive/inaturalist-cnn-classifier/best_model.pth"
+
 
     for epoch in range(num_epochs):
         train_loss, train_acc = train_one_epoch(model, train_loader, criterion, optimizer, device)
         val_loss, val_acc = validate(model, val_loader, criterion, device)
         wandb.log({
+            "epoch": epoch+1,
             "train_loss": train_loss,
             "train_acc": train_acc,
             "val_loss": val_loss,
@@ -111,14 +142,15 @@ def main():
 
             torch.save(
                 model.state_dict(),
-                "outputs/best_model.pth"
+                save_path
             )
 
             print("Best model saved")
 
         print(f"Epoch {epoch+1}/{num_epochs}")
         print(f"Train Loss : {train_loss} , Train acc : {train_acc} , Val loss : {val_loss} , Val Acc : {val_acc}")
-        
+    
+    wandb.summary["best_val_acc"] = best_val_acc
     wandb.finish()
 
 if __name__ == "__main__":
